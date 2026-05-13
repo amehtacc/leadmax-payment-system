189bad4b706fcda39e806b177c033db3222c301a
Aryan Mehta <mehtaaryan2001@gmail.com>
2026-05-13 20:30:54 +0530

    Add production database configuration

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from datetime import timedelta
+import dj_database_url
 
 load_dotenv()
 
@@ -80,20 +81,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME': os.getenv('DB_NAME'),
-
-        'USER': os.getenv('DB_USER'),
-
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-
-        'HOST': os.getenv('DB_HOST'),
-
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
 
 
